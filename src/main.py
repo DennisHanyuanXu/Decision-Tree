fcc2b7c950190cab8f9995d88bb61d3160ad4982
DennisHanyuanXu <hanyuanxu123@gmail.com>
2018-04-21 17:01:28 +0800

    add Reduced Error Pruning

--- src/main.py
+++ src/main.py
@@ -1,9 +1,55 @@
 #!/bin/env python
 #-*- encoding: utf-8 -*-
 
+import random
+import utils
+from tree import DecisionTree
+
 
 def main():
-	pass
+    '''
+    # Prepare dataset
+    headings, dataset = utils.load_dataset()
+    random.shuffle(dataset)
+
+    # Experiment 1:
+    train_data = dataset[:30000]
+    test_data = dataset[30000:40000]
+    dt = DecisionTree.build_tree(train_data, DecisionTree.entropy)
+    err = DecisionTree.count_error(dt, test_data)
+    print('Experiment 1, accuracy: %d/%d = %f' % \
+        (len(test_data) - err, len(test_data), (len(test_data) - err) / len(test_data)))
+    
+    # Experiment 2:
+    train_data = dataset[:30000]
+    test_data = dataset[30000:40000]
+    dt = DecisionTree.build_tree(train_data, DecisionTree.gini)
+    err = DecisionTree.count_error(dt, test_data)
+    print('Experiment 2, accuracy: %d/%d = %f' % \
+        (len(test_data) - err, len(test_data), (len(test_data) - err) / len(test_data)))
+    
+    # Experiment 3:
+    train_data = dataset[:15000]
+    prune_data = dataset[15000:30000]
+    test_data = dataset[30000:40000]
+    dt = DecisionTree.build_tree(train_data, DecisionTree.gini)
+    err_prune = DecisionTree.count_error(dt, prune_data)
+    err_test = DecisionTree.count_error(dt, test_data)
+    print('Experiment 3, accuracy: %d/%d = %f' % \
+        (len(prune_data) - err_prune, len(prune_data), (len(prune_data) - err_prune) / len(prune_data)))
+    print('Experiment 3, accuracy: %d/%d = %f' % \
+        (len(test_data) - err_test, len(test_data), (len(test_data) - err_test) / len(test_data)))
+    DecisionTree.plot_tree(dt, headings)
+    DecisionTree.reduced_error_pruning(dt)
+    DecisionTree.plot_tree(dt, headings)
+    err_prune = DecisionTree.count_error(dt, prune_data)
+    err_test = DecisionTree.count_error(dt, test_data)
+    print('Experiment 3, accuracy: %d/%d = %f' % \
+        (len(prune_data) - err_prune, len(prune_data), (len(prune_data) - err_prune) / len(prune_data)))
+    print('Experiment 3, accuracy: %d/%d = %f' % \
+        (len(test_data) - err_test, len(test_data), (len(test_data) - err_test) / len(test_data)))
+    '''
+    return
 
 if __name__ == '__main__':
 	main()
--- src/tree.py
+++ src/tree.py
@@ -13,7 +13,9 @@ class DecisionTree:
         self.value = value
         self.true_branch = true_branch
         self.false_branch = false_branch
+        # Classification result at current node, determined while building the tree
         self.result = result
+        # Values assigned while pruning or testing
         self.results = results
         self.error = error
 
@@ -61,6 +63,7 @@ class DecisionTree:
         result = sorted(utils.count(dataset).items(), key=lambda x: x[1], \
                         reverse=True)[0][0]
 
+        # Choose the best feature
         for i in range(feature_cnt):
 
             unique_values = list(set([data[i] for data in dataset]))
@@ -89,9 +92,10 @@ class DecisionTree:
     def plot_tree(cls, tree, headings):
 
         def tree_to_str(tree, indent='\t\t'):
+            # General output
             output = str(tree.result) + ' '
             output += str(tree.results) + ' ' if tree.results else ''
-            output += str(tree.error) + ' ' if tree.error else ''
+            output += 'err = ' + str(tree.error) + ' ' if tree.error else 'err = 0 '
 
             # Leaf node
             if not (tree.true_branch or tree.false_branch):
@@ -113,9 +117,47 @@ class DecisionTree:
 
         print(tree_to_str(tree))
 
+    @classmethod
+    def count_error(cls, tree, dataset):
+        tree.results = utils.count(dataset)
+        tree.error = 0
+        for k, v in tree.results.items():
+            if k != tree.result:
+                tree.error += v
 
-# Test
-if __name__ == '__main__':
-    headings, dataset = utils.load_dataset()
-    t = DecisionTree.build_tree(dataset[:100], DecisionTree.gini)
-    DecisionTree.plot_tree(t, headings)
+        # Leaf node
+        if not (tree.true_branch or tree.false_branch):
+            return tree.error
+
+        true_set = []
+        false_set = []
+        for data in dataset:
+            v = data[tree.feature]
+            if isinstance(v, int) or isinstance(v, float):
+                if v >= tree.value:
+                    true_set.append(data)
+                else:
+                    false_set.append(data)
+            else:
+                if v == tree.value:
+                    true_set.append(data)
+                else:
+                    false_set.append(data)
+        return cls.count_error(tree.true_branch, true_set) + cls.count_error(tree.false_branch, false_set)
+
+    @classmethod
+    def reduced_error_pruning(cls, tree):
+        # Leaf node
+        if not (tree.true_branch or tree.false_branch):
+            error = tree.error
+        else:
+            error_true = cls.reduced_error_pruning(tree.true_branch)
+            error_false = cls.reduced_error_pruning(tree.false_branch)
+            # Prune its subtree if it has less error
+            if tree.error <= error_true + error_false:
+                tree.true_branch = None
+                tree.false_branch = None
+                error = tree.error
+            else:
+                error = error_true + error_false
+        return error
