2853db4874337dc182ffa65531117a965043c4a3
DennisHanyuanXu <hanyuanxu123@gmail.com>
2018-05-09 17:40:32 +0800

    add example of training, testing and pruning a tree

--- src/main.py
+++ src/main.py
@@ -3,53 +3,63 @@
 
 import random
 import utils
+import conf
 from tree import DecisionTree
 
 
 def main():
-    '''
-    # Prepare dataset
+    # Steps to build and prune a decision tree:
+
+    # 1. Prepare dataset.
     headings, dataset = utils.load_dataset()
     random.shuffle(dataset)
+    # Split the dataset into training data, test data and pruning data if needed.
+    train_data = dataset[:32000]
+    test_data = dataset[32000:40000]
+    # prune_data = dataset[:]
 
-    # Experiment 1:
-    train_data = dataset[:30000]
-    test_data = dataset[30000:40000]
+
+    # 2. Grow a decision tree from training data based on entropy or gini.
     dt = DecisionTree.build_tree(train_data, DecisionTree.entropy)
-    err = DecisionTree.count_error(dt, test_data)
-    print('Experiment 1, accuracy: %d/%d = %f' % \
+    # dt = DecisionTree.build_tree(train_data, DecisionTree.gini)
+
+
+    # 3. Visualize the tree.
+    DecisionTree.plot_tree(dt, headings, conf.org_tree_filepath)
+    leaves = DecisionTree.count_leaves(dt)
+    print('Leaves count before pruning: %d' % leaves)
+
+
+    # 4. Run the test data through the tree.
+    err = DecisionTree.evaluate(dt, test_data)
+    print('Accuracy before pruning: %d/%d = %f' % \
         (len(test_data) - err, len(test_data), (len(test_data) - err) / len(test_data)))
+
+
+    # 5. Prune the tree.
+    #   5.1 REP: REP requires another dataset for pruning, so we need to split the dataset in a different way.
+
+    #   5.2 PP: top-down
+    DecisionTree.top_down_pessimistic_pruning(dt)
+
+    #   5.3 PP: bottom-up.
+    # DecisionTree.bottom_up_pessimistic_pruning(dt)
     
-    # Experiment 2:
-    train_data = dataset[:30000]
-    test_data = dataset[30000:40000]
-    dt = DecisionTree.build_tree(train_data, DecisionTree.gini)
-    err = DecisionTree.count_error(dt, test_data)
-    print('Experiment 2, accuracy: %d/%d = %f' % \
+    #   5.4 MEP
+    # DecisionTree.minimum_error_pruning(dt)
+
+
+    # 6. Visualize the pruned tree.
+    DecisionTree.plot_tree(dt, headings, conf.prn_tree_filepath)
+    leaves = DecisionTree.count_leaves(dt)
+    print('Leaves count after pruning: %d' % leaves)
+
+
+    # 7. Check if the classification ability is improved after pruning.
+    err = DecisionTree.evaluate(dt, test_data)
+    print('Accuracy after pruning: %d/%d = %f' % \
         (len(test_data) - err, len(test_data), (len(test_data) - err) / len(test_data)))
-    
-    # Experiment 3:
-    train_data = dataset[:15000]
-    prune_data = dataset[15000:30000]
-    test_data = dataset[30000:40000]
-    dt = DecisionTree.build_tree(train_data, DecisionTree.gini)
-    err_prune = DecisionTree.count_error(dt, prune_data)
-    err_test = DecisionTree.count_error(dt, test_data)
-    print('Experiment 3, accuracy: %d/%d = %f' % \
-        (len(prune_data) - err_prune, len(prune_data), (len(prune_data) - err_prune) / len(prune_data)))
-    print('Experiment 3, accuracy: %d/%d = %f' % \
-        (len(test_data) - err_test, len(test_data), (len(test_data) - err_test) / len(test_data)))
-    DecisionTree.plot_tree(dt, headings)
-    DecisionTree.reduced_error_pruning(dt)
-    DecisionTree.plot_tree(dt, headings)
-    err_prune = DecisionTree.count_error(dt, prune_data)
-    err_test = DecisionTree.count_error(dt, test_data)
-    print('Experiment 3, accuracy: %d/%d = %f' % \
-        (len(prune_data) - err_prune, len(prune_data), (len(prune_data) - err_prune) / len(prune_data)))
-    print('Experiment 3, accuracy: %d/%d = %f' % \
-        (len(test_data) - err_test, len(test_data), (len(test_data) - err_test) / len(test_data)))
-    '''
-    return
+
 
 if __name__ == '__main__':
 	main()
